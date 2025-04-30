091b6d6db60dd84426a1519ba6575fb43ba48bcb
Ana Karolina Ferraro <ferraroanakarolina@gmail.com>
2025-04-30 20:29:48 -0300

    Add files via upload

---
+++ aula12ex5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char senha[20];
+  printf("Digite a senha: ");
+  scanf("%19s", senha);
+
+int tamanho = strlen(senha);
+
+if (tamanho < 8) {
+    printf("Erro na senha\n");
+} else {
+printf("Senha conforme\n");
+  }
+
+  return 0;
+}
